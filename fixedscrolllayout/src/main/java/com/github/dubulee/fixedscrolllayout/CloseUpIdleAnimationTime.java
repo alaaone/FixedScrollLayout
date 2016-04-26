cb7348dfe1ac393737f2334ad78788de6c760332
mugku <mugkulee@gmail.com>
2016-04-26 13:39:44 +0900

    Update CloseUpIdleAnimationTime.java

--- fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/CloseUpIdleAnimationTime.java
+++ fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/CloseUpIdleAnimationTime.java
@@ -2,8 +2,8 @@ package com.github.dubulee.fixedscrolllayout;
 
 /**
  * CloseUpIdleAnimationTime.java
- * Created by DUBULEE on 2015/12/28.
- * Copyright (c) DUBULEE. All rights reserved.
+ * Created by mugku on 2015/12/28.
+ * Copyright (c) mugku. All rights reserved.
  */
 public interface CloseUpIdleAnimationTime {
     long compute(FixedScrollLayout layout, int nowY, int endY, int maxY);
