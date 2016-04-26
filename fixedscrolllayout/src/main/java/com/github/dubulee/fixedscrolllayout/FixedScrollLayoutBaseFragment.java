c2fc94fa5a8dfff406e8ef07df5ab799a26d9fd7
mugku <mugkulee@gmail.com>
2016-04-26 13:40:41 +0900

    Update FixedScrollLayoutBaseFragment.java

--- fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/FixedScrollLayoutBaseFragment.java
+++ fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/FixedScrollLayoutBaseFragment.java
@@ -4,8 +4,8 @@ import android.support.v4.app.Fragment;
 
 /**
  * FixedScrollLayoutBaseFragment.java
- * Created by DUBULEE on 2015/12/28.
- * Copyright (c) DUBULEE. All rights reserved.
+ * Created by mugku on 2015/12/28.
+ * Copyright (c) mugku. All rights reserved.
  */
 public abstract class FixedScrollLayoutBaseFragment extends Fragment implements CanScrollVerticallyDelegate {
     public FixedScrollLayoutBaseFragment() {
