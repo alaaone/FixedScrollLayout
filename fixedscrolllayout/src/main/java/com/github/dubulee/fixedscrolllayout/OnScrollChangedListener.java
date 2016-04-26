01699ba3e21736b45dbe1379f41cf40f7f4ec54f
mugku <mugkulee@gmail.com>
2016-04-26 13:41:25 +0900

    Update OnScrollChangedListener.java

--- fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/OnScrollChangedListener.java
+++ fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/OnScrollChangedListener.java
@@ -2,8 +2,8 @@ package com.github.dubulee.fixedscrolllayout;
 
 /**
  * OnScrollChangedListener.java
- * Created by DUBULEE on 2015/12/28.
- * Copyright (c) DUBULEE. All rights reserved.
+ * Created by mugku on 2015/12/28.
+ * Copyright (c) mugku. All rights reserved.
  */
 public interface OnScrollChangedListener {
     void onScrollChanged(int y, int oldY, int maxY);
