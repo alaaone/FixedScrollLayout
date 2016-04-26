a55054f276b50f78c74c8b156e1e1524c487fc97
mugku <mugkulee@gmail.com>
2016-04-26 13:40:04 +0900

    Update CloseUpLogic.java

--- fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/CloseUpLogic.java
+++ fixedscrolllayout/src/main/java/com/github/dubulee/fixedscrolllayout/CloseUpLogic.java
@@ -2,8 +2,8 @@ package com.github.dubulee.fixedscrolllayout;
 
 /**
  * CloseUpLogic.java
- * Created by DUBULEE on 2015/12/28.
- * Copyright (c) DUBULEE. All rights reserved.
+ * Created by mugku on 2015/12/28.
+ * Copyright (c) mugku. All rights reserved.
  */
 public interface CloseUpLogic {
     int getFlingFinalY(FixedScrollLayout layout, boolean isScrollingBottom, int nowY, int suggestedY, int maxY);
